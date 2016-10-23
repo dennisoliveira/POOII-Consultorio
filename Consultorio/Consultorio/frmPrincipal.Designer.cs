fad5e9723b74c317e395012adaec94e48cd0e655
Dennis de Oliveira <contato@dennisoliveira.com.br>
2016-10-23 15:21:08 -0200

    Configurado form principal com ícones e botões

---
+++ Consultorio/Consultorio/frmPrincipal.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Consultorio
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnMedicos = new System.Windows.Forms.ToolStripButton();
+            this.btnPacientes = new System.Windows.Forms.ToolStripButton();
+            this.btnConsultas = new System.Windows.Forms.ToolStripButton();
+            this.btnRelatorios = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.White;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnMedicos,
+            this.btnPacientes,
+            this.btnConsultas,
+            this.btnRelatorios});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(112, 411);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnMedicos
+            // 
+            this.btnMedicos.Image = ((System.Drawing.Image)(resources.GetObject("btnMedicos.Image")));
+            this.btnMedicos.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnMedicos.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnMedicos.Name = "btnMedicos";
+            this.btnMedicos.Size = new System.Drawing.Size(109, 52);
+            this.btnMedicos.Text = "Médicos";
+            // 
+            // btnPacientes
+            // 
+            this.btnPacientes.Image = ((System.Drawing.Image)(resources.GetObject("btnPacientes.Image")));
+            this.btnPacientes.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnPacientes.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPacientes.Name = "btnPacientes";
+            this.btnPacientes.Size = new System.Drawing.Size(109, 52);
+            this.btnPacientes.Text = "Pacientes";
+            // 
+            // btnConsultas
+            // 
+            this.btnConsultas.Image = ((System.Drawing.Image)(resources.GetObject("btnConsultas.Image")));
+            this.btnConsultas.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnConsultas.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnConsultas.Name = "btnConsultas";
+            this.btnConsultas.Size = new System.Drawing.Size(111, 52);
+            this.btnConsultas.Text = "Consultas";
+            // 
+            // btnRelatorios
+            // 
+            this.btnRelatorios.Image = ((System.Drawing.Image)(resources.GetObject("btnRelatorios.Image")));
+            this.btnRelatorios.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnRelatorios.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRelatorios.Name = "btnRelatorios";
+            this.btnRelatorios.Size = new System.Drawing.Size(111, 52);
+            this.btnRelatorios.Text = "Relatórios";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 411);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.IsMdiContainer = true;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consultório Médico 1.0";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        public System.Windows.Forms.ToolStripButton btnMedicos;
+        public System.Windows.Forms.ToolStripButton btnPacientes;
+        public System.Windows.Forms.ToolStripButton btnConsultas;
+        public System.Windows.Forms.ToolStripButton btnRelatorios;
+    }
+}
+
--- Consultorio/Consultorio/frmPrincipal.cs
+++ Consultorio/Consultorio/frmPrincipal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Consultorio
 {
-    public partial class Principal : Form
+    public partial class frmPrincipal : Form
     {
-        public Principal()
+        public frmPrincipal()
         {
             InitializeComponent();
         }
