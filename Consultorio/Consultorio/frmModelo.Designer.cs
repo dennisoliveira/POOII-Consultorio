959efb363f804371fe199f57dc4bdae0376c4402
Dennis de Oliveira <contato@dennisoliveira.com.br>
2016-10-26 21:33:43 -0200

    Adicionado formulário modelo completo

---
+++ Consultorio/Consultorio/frmModelo.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Consultorio
+{
+    partial class frmModelo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmModelo));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAdicionar = new System.Windows.Forms.ToolStripButton();
+            this.btnSalvar = new System.Windows.Forms.ToolStripButton();
+            this.btnExcluir = new System.Windows.Forms.ToolStripButton();
+            this.btnLocalizar = new System.Windows.Forms.ToolStripButton();
+            this.btnFechar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblMensagem = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAdicionar,
+            this.btnSalvar,
+            this.btnExcluir,
+            this.btnLocalizar,
+            this.toolStripSeparator1,
+            this.btnFechar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(529, 55);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Image = ((System.Drawing.Image)(resources.GetObject("btnAdicionar.Image")));
+            this.btnAdicionar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnAdicionar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(110, 52);
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Image = ((System.Drawing.Image)(resources.GetObject("btnSalvar.Image")));
+            this.btnSalvar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnSalvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(90, 52);
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Image = ((System.Drawing.Image)(resources.GetObject("btnExcluir.Image")));
+            this.btnExcluir.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnExcluir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(93, 52);
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnLocalizar
+            // 
+            this.btnLocalizar.Image = ((System.Drawing.Image)(resources.GetObject("btnLocalizar.Image")));
+            this.btnLocalizar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnLocalizar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLocalizar.Name = "btnLocalizar";
+            this.btnLocalizar.Size = new System.Drawing.Size(105, 52);
+            this.btnLocalizar.Text = "Localizar";
+            this.btnLocalizar.Click += new System.EventHandler(this.btnLocalizar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Image = ((System.Drawing.Image)(resources.GetObject("btnFechar.Image")));
+            this.btnFechar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnFechar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(94, 52);
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 55);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblMensagem});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 289);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(529, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 17);
+            // 
+            // frmModelo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(529, 311);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmModelo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmModelo";
+            this.Load += new System.EventHandler(this.frmModelo_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmModelo_KeyDown);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAdicionar;
+        private System.Windows.Forms.ToolStripButton btnSalvar;
+        private System.Windows.Forms.ToolStripButton btnExcluir;
+        private System.Windows.Forms.ToolStripButton btnLocalizar;
+        private System.Windows.Forms.ToolStripButton btnFechar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblMensagem;
+    }
+}
---
+++ Consultorio/Consultorio/frmModelo.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consultorio
+{
+    public partial class frmModelo : Form
+    {
+        public enum Status
+        {
+            scInserindo,
+            scNavegando,
+            scEditando
+        }
+
+        private Status sStatus;
+
+        public frmModelo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmModelo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void LimpaControles()
+        {
+            foreach (Control ctr in this.Controls)
+            {
+                if (ctr is TextBox)
+                    (ctr as TextBox).Text = "";
+
+                if (ctr is ComboBox)
+                    (ctr as ComboBox).SelectedIndex = -1;
+
+                if (ctr is ListBox)
+                    (ctr as ListBox).SelectedIndex = -1;
+
+                if (ctr is RadioButton)
+                    (ctr as RadioButton).Checked = false;
+
+                if (ctr is CheckBox)
+                    (ctr as CheckBox).Checked = false;
+
+                if (ctr is CheckedListBox)
+                {
+                    foreach (ListControl item in (ctr as CheckedListBox).Items)
+                        item.SelectedIndex = -1;
+                }
+            }
+        }
+
+        public virtual bool Salvar()
+        {
+            return false;
+        }
+
+        public virtual bool Excluir()
+        {
+            return false;
+        }
+
+        public virtual bool Localizar()
+        {
+            return false;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (Salvar())
+            {
+                sStatus = Status.scNavegando;
+                LimpaControles();
+                MessageBox.Show("Registro salvo com sucesso.", "Consultório Médico 1.0",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("O Registro não foi salvo, por favor verifique os erros!", "Consultório Médico 1.0",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (Excluir())
+            {
+                sStatus = Status.scNavegando;
+                HabilitaDesabilita(false);
+                LimpaControles();
+                MessageBox.Show("Registro excluído com sucesso.", "Consultório Médico 1.0",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("O Registro não foi excluído, por favor verifique os erros!", "Consultório Médico 1.0",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            sStatus = Status.scInserindo;
+            HabilitaDesabilita(true);
+            LimpaControles();
+        }
+
+        private void btnLocalizar_Click(object sender, EventArgs e)
+        {
+            if (Localizar())
+            {
+                sStatus = Status.scEditando;
+                HabilitaDesabilita(true);
+            }
+        }
+
+        private void HabilitaDesabilita(bool Valor)
+        {
+            // percorre os controles da tela e os habilta ou desabilita
+            // conforme o valor passado por parâmetro (Valor)
+            foreach (Control ctr in this.Controls)
+            {
+                // Verifica se o controle é um toolstrip. Se for ele continua, ou seja,
+                // ele vai para o próximo controle. (Neste caso ele habilita e nem desabilita)
+                if (ctr is ToolStrip)
+                    continue;
+
+                ctr.Enabled = Valor;
+
+                // Será habilitado somente quando for navegação
+                btnAdicionar.Enabled = (sStatus == Status.scNavegando);
+
+                btnSalvar.Enabled = (sStatus == Status.scNavegando || sStatus == Status.scInserindo);
+                btnExcluir.Enabled = (sStatus == Status.scEditando);
+                btnLocalizar.Enabled = (sStatus == Status.scNavegando);
+                btnFechar.Enabled = true;
+            }
+        }
+
+        private void frmModelo_Load(object sender, EventArgs e)
+        {
+            sStatus = Status.scNavegando;
+            HabilitaDesabilita(false);
+            LimpaControles();
+        }
+    }
+}
